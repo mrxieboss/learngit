1d1b74938de3336551c7733ef806d3820dafcb8b
mrxieboss <428616818@qq.com>
2019-04-13 00:46:59 +0800

    获取当前时间

--- src/LearnGitDemo/Program.cs
+++ src/LearnGitDemo/Program.cs
@@ -7,6 +7,7 @@ namespace LearnGitDemo
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("当前时间："+DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
             Console.ReadKey();
         }
     }
